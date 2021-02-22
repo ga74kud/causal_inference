ee9a8bdab87c08dd447a5b65d1a29c0dd1d36f30
Michael Hartmann <michaelhartmann10@googlemail.com>
2021-02-22 13:13:01 +0100

    mdp problem
    Signed-off-by: Michael Hartmann <michaelhartmann10@googlemail.com>

--- tests/test_v1.py
+++ tests/test_v1.py
@@ -3,6 +3,16 @@ import numpy as np
 import igraph as ig
 import matplotlib.pyplot as plt
 
+def neighbour_id(A, id):
+    neigh=A[id]
+    neigbours=[idx for idx, val in enumerate(neigh) if val]
+    return neigbours
+
+def find_next_node(A, id, V):
+    neigh_id = neighbour_id(A, id)
+    values=[V[i] for i in neigh_id]
+    idx=np.argmax(values)
+    return neigh_id[idx]
 
 nodes_amount=10
 value_fcn_level=100
@@ -16,4 +26,14 @@ visual_style = {}
 visual_style["vertex_label"]=[str(idx)+"/"+str(val) for idx, val in enumerate(V)]
 visual_style["vertex_label_size"]=40
 visual_style["margin"] = 60
+A=g.get_adjacency()
+next_node=2
+for wlt in range(0, 3):
+    next_node=find_next_node(A, next_node, V)
+    print("wlt="+str(wlt)+": "+ str(next_node))
+# print(A)
+#
 ig.plot(g, **visual_style)
+
+r=1
+None
---
+++ tests/test_v2.py
@@ -0,0 +1,29 @@
+import md_pro as md
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_nodes(P, **kwargs):
+    fig, ax = plt.subplots()
+    all_points=np.array([P[p] for p in P])
+    ax.scatter(all_points[:, 0], all_points[:, 1], c=kwargs["U"])
+    for i, txt in enumerate(P):
+        ax.annotate(txt, (all_points[i, 0], all_points[i, 1]))
+
+mygrid={"x_grid": 5, "y_grid": 4}
+amount_nodes=np.int(mygrid["x_grid"]*mygrid["y_grid"])
+
+#start point
+strt_pnt='0'
+# points
+P=md.get_meshgrid_points(**mygrid)
+# Topology
+T, S = md.get_simple_topology_for_regular_grid(P, **mygrid)
+# rewards
+R = {'16': 100}
+mdp_challenge = {'S': S, 'R': R, 'T': T, 'P': P, 'gamma': .9}
+# start Markov Decision Process
+dict_mdp=md.start_mdp(mdp_challenge)
+# plot the nodes
+plot_nodes(P, **{'U': dict_mdp['U']})
+plt.show()
+None
