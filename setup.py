9f18f5c9ee9b3e217bd5d63fb15c1966db9ec0e2
Michael Hartmann <michaelhartmann10@googlemail.com>
2021-02-24 16:10:07 +0100

    Signed-off-by: Michael Hartmann <michaelhartmann10@googlemail.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='causal_inference',
-      version='0.0.3',
+      version='0.0.4',
       description='Causal Inference',
       long_description=long_description,
       long_description_content_type="text/markdown",
