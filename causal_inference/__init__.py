80bc020237d4073f7d574964c769f801da0cc22a
Michael Hartmann <michaelhartmann10@googlemail.com>
2021-03-05 11:26:31 +0100

    some new things

--- causal_inference/__init__.py
+++ causal_inference/__init__.py
@@ -17,19 +17,32 @@ def get_scm_solution(problem, input):
     # topology=obj.get_topology_by_scm(problem)
     all_exp, all_var = obj.get_scm_function(problem, input)
     return all_exp, all_var
-
+def product_two_gaussians(gaus_1, gaus_2):
+    mu_1 = gaus_1["mean"]
+    mu_2 = gaus_2["mean"]
+    dev_1 = gaus_1["dev"]
+    dev_2 = gaus_2["dev"]
+    new_mean=(mu_1/(2*dev_1**2)+mu_2/(2*dev_2**2))/(1/(2*dev_1**2)+1/(2*dev_2**2))
+    dev=(dev_1 ** 2 * dev_2**2)/(dev_1 ** 2 + dev_2**2)
+    gaus={"mean": new_mean, "dev": np.sqrt(dev)}
+    return gaus
+def sum_two_gaussians(gaus_1, gaus_2):
+    mu_1 = gaus_1["mean"]
+    mu_2 = gaus_2["mean"]
+    dev_1 = gaus_1["dev"]
+    dev_2 = gaus_2["dev"]
+    new_mean=mu_1+mu_2
+    dev=dev_1 ** 2 * dev_2**2
+    gaus={"mean": new_mean, "dev": np.sqrt(dev)}
+    return gaus
 def next_position_scm(xy, probs):
-    new_xv = {"mean": np.zeros((np.size(xy["mean"], 0), np.size(xy["mean"], 1))),
-              "dev": np.zeros((np.size(xy["dev"], 0), np.size(xy["dev"], 1)))}
-    # mean value computation
-    for row_idx in range(0, np.size(new_xv["mean"], 0)):
-        new_xv["mean"][row_idx, 0] = probs['A'][0] * xy["mean"][row_idx, 0] + probs['B'][0] * xy["mean"][row_idx, 1]
-        new_xv["mean"][row_idx, 1] = probs['C'][0] * xy["mean"][row_idx, 0] + probs['D'][0] * xy["mean"][row_idx, 1]
-
-    # deviation computation
-    for row_idx in range(0, np.size(new_xv["dev"], 0)):
-        new_xv["dev"][row_idx, 0] = probs['A'][1] ** 2 * xy["dev"][row_idx, 0] ** 2 + probs['B'][1] ** 2 * xy["dev"][
-            row_idx, 1] ** 2
-        new_xv["dev"][row_idx, 1] = probs['C'][1] ** 2 * xy["dev"][row_idx, 0] ** 2 + probs['D'][1] ** 2 * xy["dev"][
-            row_idx, 1] ** 2
-    return new_xv
+    # x-coordinate
+    prod_1 = product_two_gaussians(probs['A'], xy[0])
+    prod_2 = product_two_gaussians(probs['B'], xy[0])
+    new_x = sum_two_gaussians(prod_1, prod_2)
+    # y-coordinate
+    prod_3 = product_two_gaussians(probs['C'], xy[1])
+    prod_4 = product_two_gaussians(probs['D'], xy[1])
+    new_y = sum_two_gaussians(prod_3, prod_4)
+    new_xy=[new_x, new_y]
+    return new_xy
--- tests/test_v4.py
+++ tests/test_v4.py
@@ -1,8 +1,8 @@
 import causal_inference as ci
 import numpy as np
-probs={'A': [1, 0.1], 'B': [1, 0.1], 'C': [1, 0.1], 'D': [1, 0.1]}
-xy={"mean": np.array([[1, 2], [2, 4]]), "dev": np.array([[.1, .2], [.2, .4]])}
-
+probs={'A': {"mean": 1, "dev": 1}, 'B': {"mean": 1, "dev": 0.1}, 'C': {"mean": 1, "dev": 0.1}, 'D': {"mean": 1, "dev": 0.1}}
+x={"mean": 1, "dev": .1}
+y={"mean": 2, "dev": .2}
+xy=[x, y]
 new_xv=ci.next_position_scm(xy, probs)
-print(new_xv["mean"])
-print(new_xv["dev"])
+print(new_xv)
